ba6c8f77778053cb581557dad8161c676ac23295
Yoga Nugraha Pryandika <yogapryandika@gmail.com>
2020-09-28 12:38:56 +0700

    init project

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p>App to search Github users</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
